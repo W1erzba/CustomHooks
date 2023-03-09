5eec73f6775c04ad380aef2f003442610bdf4f7c
[Bartosz Wierzbicki] <101505041+W1erzba@users.noreply.github.com>
2023-03-09 11:21:22 +0100

    configuring our  Custom Hook.

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -1,15 +1,19 @@
 import { useState, useEffect } from 'react';
 
-const useCounter = () => {
+const useCounter = (forwards = true) => {
 	const [counter, setCounter] = useState(0);
 
 	useEffect(() => {
 		const interval = setInterval(() => {
-			setCounter((prevCounter) => prevCounter + 1);
+			if (forwards) {
+				setCounter((prevCounter) => prevCounter + 1);
+			} else {
+				setCounter((prevCounter) => prevCounter - 1);
+			}
 		}, 1000);
 
 		return () => clearInterval(interval);
-	}, []);
+	}, [forwards]); // *[forwards]* when we are adding an parameters we have to add them to the dependencies
 
 	return counter;
 };
@@ -17,3 +21,4 @@ const useCounter = () => {
 export default useCounter;
 
 // Side note: this is just a equivalent of vanila javaScript functions xD
+// It was just a simple example, introduction to custom hook world.
